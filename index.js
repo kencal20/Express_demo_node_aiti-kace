f191c94c4f8e385f80fcdccd0e9cad4786471795
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-10-02 20:06:50 +0000

    implementing  express custom middleware, built in middleware and calling helmet and morgan

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const Joi = require('joi');
 const express = require('express');
+const helmet = require('helmet')
+const morgan = require('morgan');
+const logger = require('./logger');
+const auth = require('./auth');
 const app = express();
 
-app.use(express.json());
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(helmet());
+app.use(morgan('tiny'));
+
+
+app.use(logger);
+app.use(auth)
 const courses = [
     { id: 1, name: "course1" },
     { id: 2, name: "course2" },
