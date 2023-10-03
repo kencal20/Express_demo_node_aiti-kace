174fc8ea3b9c5cc225e6ce2027d4dfa4d096327a
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-10-03 10:20:08 +0000

    using callbacks in async functions

--- async.js
+++ async.js
@@ -1,9 +1,14 @@
 console.log('Before');
-getUser(1); 
-console.log('After')
-function getUser(id) {
+getUser(1, (user) => {
+    console.log('User', user);
+});
+console.log('After');
+
+function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a db....  ')
-        return{id:id,gutHubUserName:'kencal20'}
+        callback({ id: id, gutHubUserName: 'kencal20' });
     }, 2000);
+
 }
+
