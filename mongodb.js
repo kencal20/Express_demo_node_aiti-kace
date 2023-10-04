1c6f93607a4f6f72afba5357dc666e73d59341a1
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-10-04 20:37:24 +0000

    how to create a schema and model and push it to database

--- mongodb.js
+++ mongodb.js
@@ -1,3 +1,31 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/playground ')
+    .then(() => console.log('connected to mongodb'))
+    .catch(err => console.error('could not connect the db'))
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tag: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+})
+const Course = mongoose.model('Course', courseSchema)
+
+async function createCourse() {
+
+    const course = new Course({
+        name: 'Node.js Course',
+        author: 'Ken',
+        tag: ['node', 'backend'],
+        isPublished: true
+    })
+
+    const result = await course.save();
+    console.log(result);
+
+}
+
+createCourse();
